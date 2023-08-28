24ed968cb7a2e371daff7fed367d485a15a53ee3
caioNunesS <caiogrb@gmail.com>
2023-08-28 14:28:20 -0300

    fix: teste de coupons

--- src/modules/coupons/coupon.schema.spec.ts
+++ src/modules/coupons/coupon.schema.spec.ts
@@ -15,85 +15,85 @@ describe('register coupon schema', () => {
 
       expect(schema.success).toBe(true);
     });
+  });
 
-    describe('when a invalid body', () => {
-      it('should return an error for a body without "title"', () => {
-        const invalidInput = {
-          body: {
-            description: 'description',
-            value: '0',
-          },
-        };
-        const schema = couponSchema.safeParse(invalidInput);
-        if (!schema.success) {
-          expect(schema.error.issues[0].message).toEqual(
-            'Title é um campo obrigatório'
-          );
-        }
-      });
-
-      it('should return an error for a body without "description"', () => {
-        const invalidInput = {
-          body: {
-            title: 'title',
-            value: '0',
-          },
-        };
-        const schema = couponSchema.safeParse(invalidInput);
-        if (!schema.success) {
-          expect(schema.error.issues[0].message).toEqual(
-            'Description é um campo obrigatório'
-          );
-        }
-      });
-
-      it('should return an error for a body without "value"', () => {
-        const invalidInput = {
-          body: {
-            title: 'title',
-            description: 'description',
-          },
-        };
-        const schema = couponSchema.safeParse(invalidInput);
-        if (!schema.success) {
-          expect(schema.error.issues[0].message).toEqual(
-            'Value é um campo obrigatório'
-          );
-        }
-      });
-
-      it('should return an error if "value" is not string', () => {
-        const invalidInput = {
-          body: {
-            title: 'title',
-            description: 'description',
-            value: 0,
-          },
-        };
-        const schema = couponSchema.safeParse(invalidInput);
-        if (!schema.success) {
-          expect(schema.error.issues[0].message).toEqual(
-            'Expected string, received number'
-          );
-        }
-      });
+  describe('when a invalid body', () => {
+    it('should return an error for a body without "title"', () => {
+      const invalidInput = {
+        body: {
+          description: 'description',
+          value: '0',
+        },
+      };
+      const schema = couponSchema.safeParse(invalidInput);
+      if (!schema.success) {
+        expect(schema.error.issues[0].message).toEqual(
+          'Title é um campo obrigatório'
+        );
+      }
     });
 
-    it('should return an error if "value" is not a válid number', () => {
+    it('should return an error for a body without "description"', () => {
+      const invalidInput = {
+        body: {
+          title: 'title',
+          value: '0',
+        },
+      };
+      const schema = couponSchema.safeParse(invalidInput);
+      if (!schema.success) {
+        expect(schema.error.issues[0].message).toEqual(
+          'Description é um campo obrigatório'
+        );
+      }
+    });
+
+    it('should return an error for a body without "value"', () => {
       const invalidInput = {
         body: {
           title: 'title',
           description: 'description',
-          value: 'ab',
         },
       };
       const schema = couponSchema.safeParse(invalidInput);
-
       if (!schema.success) {
-        expect(schema.error.issues[0].message).toBe(
-          'O campo "value" deve ser um número válido, com no máximo duas casas decimáis'
+        expect(schema.error.issues[0].message).toEqual(
+          'Value é um campo obrigatório'
         );
       }
     });
+
+    it('should return an error if "value" is not string', () => {
+      const invalidInput = {
+        body: {
+          title: 'title',
+          description: 'description',
+          value: 0,
+        },
+      };
+      const schema = couponSchema.safeParse(invalidInput);
+      if (!schema.success) {
+        expect(schema.error.issues[0].message).toEqual(
+          'Expected string, received number'
+        );
+      }
+    });
+  });
+
+  it('should return an error if "value" is not a válid number', () => {
+    const invalidInput = {
+      body: {
+        title: 'title',
+        description: 'description',
+        value: 'ab',
+      },
+    };
+    const schema = couponSchema.safeParse(invalidInput);
+
+    if (!schema.success) {
+      expect(schema.error.issues[0].message).toBe(
+        'O campo "value" deve ser um número válido, com no máximo duas casas decimáis'
+      );
+    }
   });
 });
