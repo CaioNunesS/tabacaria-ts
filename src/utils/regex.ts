c71ee0dac824d6ca09e45456c3c514fae4672c21
caioNunesS <caiogrb@gmail.com>
2023-10-18 13:39:47 -0300

    fix: arrumando regex para validar celular

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,7 +1,7 @@
 export const passwordRegex =
   /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{6})$/;
 
-export const phoneRegex = /^(\+55\s?)?(\d{2}\s?)?9\d{8}$/;
+export const phoneRegex = /^(\d{2})(9\d{8})$/;
 
 export const emailRegex = /^[\w\.-]+@[\w\.-]+\.\w+$/;
 
